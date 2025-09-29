51feb3df5fec7a23ff58f4f77ae5430efaf3868f
HaRiYuVaN <imnotbobforsure@gmail.com>
2025-09-29 21:46:09 +0530

    first commit

---
+++ househelp/views.py
@@ -0,0 +1,110 @@
+from typing import Any
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.http import HttpResponse
+from django.views.generic.base import TemplateView
+from django.core.mail import EmailMessage
+from django.conf import settings
+from django.contrib import messages
+from .models import Appointment
+from django.views.generic import ListView 
+import datetime
+from django.template import Context
+from django.template.loader import render_to_string, get_template
+
+
+class HomeTemplateView(TemplateView):
+    template_name = "index.html"
+
+    def post(self, request):
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        message = request.POST.get("message")
+
+
+        email = EmailMessage(
+            subject=f"From {name} to Ai.Amma tech team.",
+            body=message,
+            from_email=settings.EMAIL_HOST_USER,
+            to=[settings.EMAIL_HOST_USER],
+            reply_to=[email]
+        )
+        email.send()
+        return HttpResponse("Email sent")
+    
+class AppointmentTemplateView(TemplateView):
+    template_name = "appointment.html"
+
+    def post(self, request):
+        fname = request.POST.get("fname")
+        lname = request.POST.get("lname")
+        email = request.POST.get("email")
+        message = request.POST.get("request")
+        mobile = request.POST.get("mobile")
+
+
+        email = EmailMessage(
+            subject=f"Appointment for {fname}.",
+            body=message,
+            from_email=settings.EMAIL_HOST_USER,
+            to=[settings.EMAIL_HOST_USER],
+            reply_to=[email]
+        )
+        email.send()
+
+        appointment = Appointment.objects.create(
+            first_name=fname,
+            last_name=lname,
+            email=email,
+            phone=mobile,
+            request=message,
+        )
+        appointment.save()
+
+        
+        #return HttpResponse("Appointment sucessfully created")
+
+        messages.add_message(request, messages.SUCCESS,f"Thanks {fname}, Appointment sucessfully created")
+        return HttpResponseRedirect(request.path)
+    
+class ManageAppointmentTemplateView(ListView):
+    template_name = "manage-appointments.html"
+    model = Appointment
+    context_object_name = "appointments"
+    login_required = True
+    paginate_by = 3
+
+    def post(self, request):
+        date = request.POST.get("date")
+        appointment_id = request.POST.get("appointment-id")
+        appointment = Appointment.objects.get(id=appointment_id)
+        appointment.accepted=True
+        appointment.accepted_date = datetime.datetime.now()
+        appointment.save()
+
+        data = {
+            "fname":appointment.first_name,
+            "date":date,
+        }
+
+        message = get_template('email.html').render(data)
+        email = EmailMessage(
+            "About your appointment",
+            message,
+            settings.EMAIL_HOST_USER,
+            [appointment.phone],
+        )
+        email.content_subtype = "html"
+        email.send()
+
+
+        messages.add_message(request, messages.SUCCESS, f"You accepted {appointment.first_name}'s appointment on {date}")
+        return HttpResponseRedirect(request.path)
+
+    def get_context_data(self,*args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        appointments = Appointment.objects.all()
+        context.update({   
+            "title":"Manage Appointments"
+        })
+        return context
